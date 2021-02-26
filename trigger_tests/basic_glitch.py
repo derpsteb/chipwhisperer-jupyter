5e82ad28d0075cc420df371784ff20ac7d1f98c9
Otto Bittner <otto-bittner@gmx.de>
2021-02-26 16:02:17 +0100

    Modify to profile for skipping str instructions

--- trigger_tests/basic_glitch.py
+++ trigger_tests/basic_glitch.py
@@ -3,6 +3,7 @@ import serial
 import Setup_Generic
 
 import matplotlib.pyplot as plt
+import random
 
 def setup_cw(scope, offset, nr_samples):
     scope.default_setup()
@@ -54,31 +55,42 @@ if __name__ == "__main__":
 
     scope, target, prog = Setup_Generic.setup(version=None, platform=PLATFORM)
     # Initialize connection to ARTY A7 FPGA
-    fpga = serial.Serial("/dev/ttyUSB3", baudrate=115200)
-    target = serial.Serial("/dev/ttyUSB1", baudrate=115200, timeout=0.2)
+    fpga = serial.Serial("/dev/ttyUSB2", baudrate=115200)
+    target = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=0.2)
 
     offset = 0
     nr_samples = 24400
     
     setup_basic(scope, offset, nr_samples)
     setup_glitcher(scope)
+    chipfail_lib.setup(fpga)
     scope.adc.decimate = 50
 
-    MIN_OFFSET = 300
-    MAX_OFFSET = 3000
+    MIN_OFFSET = 1160
+    MAX_OFFSET = 1161
     OFFSET_STEP = 1
     MIN_WIDTH = 1160
-    MAX_WIDTH = 1164
+    MAX_WIDTH = 1161
     WIDTH_STEP = 1
-
 
     success = False
     while not success:
-        for offset in range(MIN_OFFSET, MAX_OFFSET, OFFSET_STEP):
-            for width in range(MIN_WIDTH, MAX_WIDTH, WIDTH_STEP):
-                chipfail_lib.setup(fpga, delay=offset, glitch_pulse=width)
+        width_range = list(range(MIN_WIDTH, MAX_WIDTH, WIDTH_STEP))
+        random.shuffle(width_range)
+        for width in width_range:
+            chipfail_lib.cmd_uint32(fpga, chipfail_lib.CMD_SET_GLITCH_PULSE, width)
+
+            offset_range = list(range(MIN_OFFSET, MAX_OFFSET, OFFSET_STEP))
+            # random.shuffle(offset_range)
+            for offset in offset_range:
+                chipfail_lib.cmd_uint32(fpga, chipfail_lib.CMD_SET_DELAY, offset)
+
                 trace = collect_trace_basic()    
                 # plt.plot(trace)
                 # plt.show()
-                success = chipfail_lib.success_uart(target, offset, width)
+                success = chipfail_lib.success_uart(target, offset, width, b'target_ptr: 52012cc8 | val: 42424242\n', False)
+                if success:
+                    exit(0)
+                chipfail_lib.flush_uart(target)
+
                 chipfail_lib.wait_until_rdy(fpga)
